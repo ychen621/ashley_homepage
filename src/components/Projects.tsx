f5b6dc4beef68a93ced3583ed6dd3f4faa9c7776
ychen621 <ychen621@usc.edu>
2024-06-24 20:20:28 -0700

    publication and project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,36 @@ function Projects(){
             <div className="projects_parent">
                 <h2 className="title">Projects</h2>
                 <div id="container">
-                    
+                    <ul className="projectul">
+                        <li className="projectList">
+                            <h3 className="pTitle">Lisence Plate Recognizor</h3>
+                            <h4 className="pSubTitle">Multithread | Java, Git</h4>
+                        </li>
+                        <li className="projectList">
+                            <h3 className="pTitle">Artist Search</h3>
+                            <h4 className="pSubTitle">Web Development | Python, JavaScript, TypeScript, Angular, BootStrap, Node.js, Express, Flask, Google Cloud Platform</h4>
+                        </li>
+                        <li className="projectList">
+                            <h3 className="pTitle">Meeting Scheduling System</h3>
+                            <h4 className="pSubTitle">Socket | C++, TCP, UDP</h4>
+                        </li>
+                        <li className="projectList">
+                            <h3 className="pTitle">A Journey of Ice and Fire</h3>
+                            <h4 className="pSubTitle">Game Development | C#, Unity, Figma, Git</h4>
+                        </li>
+                        <li className="projectList">
+                            <h3 className="pTitle">Weenix Kernel</h3>
+                            <h4 className="pSubTitle">Operating System | C, Git</h4>
+                        </li>
+                        <li className="projectList">
+                            <h3 className="pTitle">Cat-Dog Differentiator</h3>
+                            <h4 className="pSubTitle">Machine Learning | Python, TensorFlow, Keras</h4>
+                        </li>
+                        <li className="projectList">
+                            <h3 className="pTitle">Heterogenous Breast Cancer Cells Interaction</h3>
+                            <h4 className="pSubTitle">Mathematical Modeling | Python</h4>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </>
